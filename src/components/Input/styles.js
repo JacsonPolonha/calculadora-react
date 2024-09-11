d6980a6f313b01b4c494d3dec16e75077458f21f
JacsonPolonha <jacsonpolonha@gmail.com>
2024-09-11 01:03:42 -0300

    commit de arquivos

---
+++ src/components/Input/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 76px;
+    background-color: #aaffaa;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 76px;
+        background-color: #aaffaa;
+        border: 0;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+    }
+`
